ca9fac020d5eef3648448a7d76bafbb55561606f
Apple03244 <apple03244@gmail.com>
2024-02-27 01:31:38 +0900

    삭제

--- connect_mysql_test.py
+++ connect_mysql_test.py
@@ -13,7 +13,7 @@ with open('./index.txt',"r") as f:
 config = {
     'user': 'admin',
     'password': 'lunchlab0219',
-    'host': 'lunch-lab-db.c7gaq8m06dlk.ap-southeast-2.rds.amazonaws.com',
+    'host': "",
     'database': 'test',
     'raise_on_warnings': True
 }
@@ -27,8 +27,9 @@ cursor=conn.cursor() # 커서 만들기
 
 
 # 해야할일 -> 남아있는 매장 index 가져오기
-cursor.execute("select ")
-
+cursor.execute("show tables")
+result=cursor.fetchall()
+print(result)
 
 
 # index 파일 최신화 -> 깃 엣션으로 넘어갈 가능성이 있음..
