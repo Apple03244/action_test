69a6dda2ed03a32bf7dc17d2681ce2758020097a
Apple03244 <apple03244@gmail.com>
2024-05-15 17:52:27 +0900

    동기화

--- index_control_sql.py
+++ index_control_sql.py
@@ -10,7 +10,7 @@ test=json.loads(input)
 test_connect=mysql.connect(**test)
 corr=test_connect.cursor()
 
-#변경가능(index 마지막 값만 불러옴)
+# 변경가능(index 마지막 값만 불러옴)
 corr.execute('''
              use ORIGINAL;
              select id from origin order by id desc limit 1;                          
@@ -19,7 +19,10 @@ last_index=corr.fetchall()
 corr.close()
 
 # 아키텍쳐
-index_dict={}
-temp=last_index/20
-for i in range(20):
-    index_dict[i]=
+index_dict=dict.fromkeys([f"idx_{i}" for i in range(1,21)])
+x,y=divmod(last_index,20)
+index_list=[range(20*i+1,20*(i+1)+1) for i in range(x)]+[range(20*x,last_index+1)]
+
+
+
+# 
